ba095a2ae7e491c2d950fc8959e392848c858d19
johnatas <johnatasr@hotmail.com>
2019-11-11 15:46:13 -0300

    Editing Filters

--- mediar/filters.py
+++ mediar/filters.py
@@ -1,4 +1,7 @@
 import pyodbc
+import datetime
+from itertools import groupby
+from collections import Counter, OrderedDict
 
 data = []
 
@@ -25,25 +28,52 @@ def mediar_data():
     cursor.close()
 
     return data
+#
+#
+# def tabela_graph():
+#
+#     total= []
+#     acessos = mediar_data()
+#     while True:
+#         objetos = [dict(nome=acesso['first_name'], sobrenome=acesso['last_name'], ultimo_acesso=acesso['last_login']) for acesso in acessos]
+#         count = [len(list(group)) for key, group in groupby(objetos)]
+#
+#         # for obj in zip(objetos, count):
+#         #     return obj
+#         total = list(zip(objetos, count))
+#         return total
+#
+# def graphQntUsuario():
+#
+#     acessos = mediar_data()
+#     nomes = [acesso["first_name"] for acesso in acessos]
+#     countnomes = Counter(nomes)
+#
+#     # s = [(k, countnomes[k]) for k in sorted(countnomes, key=countnomes.get, reverse=True)]
+#
+#     # for k, v in s:
+#     #     count = zip(k,v)
+#
+#     total = sum(countnomes.values())
+#
+#     return countnomes, total
+#
+# def graphQntHora():
+#
+#     data = mediar_data()
+#     horas = [acesso['last_login'] for acesso in data]
+#     formated = sorted([hora.strftime("%H:%M") for hora in horas])
+#
+#     return formated
+
+def graphAcessoPeriodo():
+    data = mediar_data()
 
 
-def tabela_graph():
-
-    total= []
-    acessos = mediar_data()
-    while True:
-        objetos = [dict(nome=acesso['first_name'], sobrenome=acesso['last_name'], ultimo_acesso=acesso['last_login']) for acesso in acessos]
-
-        for nome in objetos['nome']:
-            aux = ''
-
-            sorted(dicionario, key=lambda k: k['nome_da_coluna'])
 
 
+    print(data)
 
-        return objetos
-
-
-nomes = tabela_graph()
+nomes = graphAcessoPeriodo()
 print(nomes)
 
